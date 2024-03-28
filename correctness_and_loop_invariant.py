ee4768193ce2c979dd95f70f69a775b41e3e4e65
lasith54 <lasithperera254@gmail.com>
2024-03-28 23:48:50 +0530

    Correctness and the Loop Invariant

---
+++ correctness_and_loop_invariant.py
@@ -0,0 +1,17 @@
+def insertionSort(N, arr):
+    for i in range (1,N):
+        value = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > value:
+            arr[j+1] = arr[j]
+            j = j - 1
+        arr[j+1] = value
+
+    for j in range (0,N):
+        print(arr[j])
+
+N = int(input())
+arr = [int(x) for x in input().split()]
+
+insertionSort(N, arr)
+
